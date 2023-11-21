56a14526009b120fab7c77a61a5523debb8c8668
Clarissa Kümhof <clarissakuemhof@AirvonClarissa.usilu.net>
2023-11-21 14:57:02 +0100

    created first draft of achievements page

--- HikeUnite_App/app/src/main/java/com/example/stepappv4/ui/Achievements/AchievementsFragment.java
+++ HikeUnite_App/app/src/main/java/com/example/stepappv4/ui/Achievements/AchievementsFragment.java
@@ -8,9 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.stepappv4.R;
 import com.example.stepappv4.databinding.FragmentAchievementsBinding;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 public class AchievementsFragment extends Fragment {
+
+    private LinearProgressIndicator progressBar;
 
     private FragmentAchievementsBinding binding;
 
@@ -19,6 +23,34 @@ public class AchievementsFragment extends Fragment {
 
         binding = FragmentAchievementsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        progressBar = (LinearProgressIndicator) root.findViewById(R.id.achieveProgress);
+        progressBar.setMax(5000);
+        progressBar.setProgress(342);
+
+        progressBar = (LinearProgressIndicator) root.findViewById(R.id.achieveProgress2);
+        progressBar.setMax(10);
+        progressBar.setProgress(3);
+
+        progressBar = (LinearProgressIndicator) root.findViewById(R.id.achieveProgress3);
+        progressBar.setMax(100);
+        progressBar.setProgress(32);
+
+        progressBar = (LinearProgressIndicator) root.findViewById(R.id.achieveProgress4);
+        progressBar.setMax(5000);
+        progressBar.setProgress(2756);
+
+        progressBar = (LinearProgressIndicator) root.findViewById(R.id.achieveProgress5);
+        progressBar.setMax(100);
+        progressBar.setProgress(89);
+
+        progressBar = (LinearProgressIndicator) root.findViewById(R.id.achieveProgress6);
+        progressBar.setMax(100);
+        progressBar.setProgress(67);
+
+        progressBar = (LinearProgressIndicator) root.findViewById(R.id.achieveProgress7);
+        progressBar.setMax(5000);
+        progressBar.setProgress(4565);
 
         return root;
     }
