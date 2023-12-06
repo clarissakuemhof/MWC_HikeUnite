d0d46b5146892ec5fefaab4f30774002d3664990
70NA5 <56596204+70NA5@users.noreply.github.com>
2023-12-06 18:19:06 +0100

    added location call and permission

--- HikeUnite_App/app/src/main/java/com/example/stepappv4/ui/GPS/GPSTestFragment.java
+++ HikeUnite_App/app/src/main/java/com/example/stepappv4/ui/GPS/GPSTestFragment.java
@@ -1,12 +1,16 @@
 package com.example.stepappv4.ui.GPS;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,13 +19,22 @@ import com.example.stepappv4.R;
 import com.example.stepappv4.databinding.FragmentAchievementsBinding;
 import com.example.stepappv4.databinding.FragmentDetailsBinding;
 import com.example.stepappv4.databinding.FragmentGpstestBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
+
+import java.util.Objects;
 
 
 public class GPSTestFragment extends Fragment {
 
 
     ImageView icon;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+
 
     private FragmentGpstestBinding binding;
 
@@ -32,10 +45,42 @@ public class GPSTestFragment extends Fragment {
         binding = FragmentGpstestBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
+
+        if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request the permission
+            ActivityCompat.requestPermissions(requireActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+
+            // The onRequestPermissionsResult method will be called asynchronously
+            // to handle the user's response to the permission request.
+
+        } else {
+            // Permission has already been granted
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations, this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                            }
+                        }
+                    });
+        }
+
+
+
+
+
 
 
         return root;
     }
+
+
 
 
 
