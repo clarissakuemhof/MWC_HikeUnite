678949d61f956249462bf7290edc51f8e6b30d85
aasoin <aasoin@utu.fi>
2023-12-14 19:00:54 +0100

    alpha 0.4 -> 0.2

--- HikeUnite_App/app/src/main/java/com/example/stepappv4/ui/Home/HomeFragment.java
+++ HikeUnite_App/app/src/main/java/com/example/stepappv4/ui/Home/HomeFragment.java
@@ -162,7 +162,7 @@ class  StepCounterListener implements SensorEventListener{
     private double smoothAccMag = 0;
     // the value requires imperative testing
     // value is between 0 and 1
-    private double alpha = 0.4;
+    private double alpha = 0.2;
     private int lastAddedIndex = 1;
     int stepThreshold = 6;
 
